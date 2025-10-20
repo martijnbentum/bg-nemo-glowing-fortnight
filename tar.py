12cb273a1dee107f0d4e541c0e5e1188d60d9071
martijnbentum <bentummartijn@gmail.com>
2025-10-20 15:27:40 +0200

    added multiprocessing

--- tar.py
+++ tar.py
@@ -1,5 +1,29 @@
+from functools import partial
+from multiprocessing import Pool
 from progressbar import progressbar
 import tarfile
+import time
+from tqdm import tqdm
+
+def _runner(args, output_path, verbose):
+    tar_file_path, member_names = args
+    return extract_files(tar_file_path, member_names, output_path, verbose)
+
+def extract_files_with_tar_audio_file_dict_mp(tar_audio_file_dict,
+    output_path, num_workers = 64, verbose = False):
+    items = tar_audio_file_dict.items()
+    print(time.ctime(), len(items), num_workers)
+    with Pool(processes=num_workers) as pool:
+        results = []
+        for r in tqdm(
+            pool.imap_unordered(
+                partial(_runner, output_path = output_path, verbose = verbose),
+                items, chunksize = num_workers), 
+                total= len(items)):
+            results.append(r)
+            print(r, 'done', len(results), len(items), time.ctime())
+
+        
 
 def extract_files_with_tar_audio_file_dict(tar_audio_file_dict, output_path, 
     verbose = False):
@@ -9,6 +33,7 @@ def extract_files_with_tar_audio_file_dict(tar_audio_file_dict, output_path,
 def extract_files(tar_file_path, member_names, output_path, verbose = False):
     with tarfile.open(tar_file_path, 'r') as tar:
         extract_files_from_tar(tar, member_names, output_path, verbose)
+    return tar_file_path
 
 def list_contents(tar_file_path):
     print('listing files from tar:', tar_file_path)
@@ -26,7 +51,7 @@ def extract_file_from_tar(tar, member_name, output_path, verbose = False):
     if verbose:
         print(f'extracting {member_name} from {tar_file_path} to {output_path}')
     member = tar.getmember(member_name)
-    tar.extract(member, path=output_path)
+    tar.extract(member, path=output_path, filter = 'data')
 
 def extract_files_from_tar(tar, member_names, output_path, verbose = False):
     for member_name in member_names:
